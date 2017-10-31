8f9e39742c98ac61196057031fd4b02527e600b8
jorge jardines <jorge@themotion.com>
2017-10-31 01:50:29 +0100

    Add template for view page

--- wiki/wiki.go
+++ wiki/wiki.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
@@ -29,7 +28,7 @@ func loadPage(title string) (*Page, error) {
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,7 +37,11 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 			p = &Page{Title: title}
 	}
-	t, _ := template.ParseFiles("edit.html")
+	renderTemplate(w, "edit", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
 	t.Execute(w, p)
 }
 
